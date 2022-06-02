eac79fe82793f84b196b2a9e9edf86c08ac0ecee
YESIMGOD <ristoo2001@gmail.com>
2022-06-03 00:18:41 +0530

    Enhanced Code

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -28,20 +28,24 @@ public class MainActivity extends AppCompatActivity {
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
     String[] operators ={"+","-","*","/"};
     int correctButton=0; //which button will have the correct answer (tag of that button)
-    Random random=new Random(); //You will generate randdom alegebra questions
+    Random random=new Random(); //You will generate random algebra questions
     TextView textView2;
     Button button1,button2,button3,button4;
-    public void load(View view){
+    public void load(View view)
+    {
         Button buttonClicked=(Button)view;
-        if(buttonClicked.getTag().toString().equals(correctButton+"")){
+        if(buttonClicked.getTag().toString().equals(correctButton+""))
+        {
             score[matchCounter++]=1;
-        }else{
+        }else
+        {
             score[matchCounter++]=0;
         }
         newMatch();
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1=findViewById(R.id.button1);
@@ -51,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         textView2=findViewById(R.id.textView2);
         newMatch();
         sharedPreferences=this.getSharedPreferences("com.example.aiapp_2022", Context.MODE_PRIVATE);
-        int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimenssional array
+        int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimensional array
         double slope=LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
         new AlertDialog.Builder(this)
                 // .setIcon() //your custom icon
@@ -65,32 +69,70 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2=random.nextInt(10);
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-        // Your code here, to diplay correct and incorrect options on the buttons
+        // Your code here, to display correct and incorrect options on the buttons
+        int correct_answer;
+        switch (operator) {
+            case "+":
+                correct_answer = operand1 + operand2;
+                break;
+            case "-":
+                correct_answer = operand1 - operand2;
+                break;
+            case "*":
+                correct_answer = operand1 * operand2;
+                break;
+            default:
+                correct_answer = operand1 / operand2;
+                break;
+        }
 
-        if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
-
+        if(correctButton == 0)
+        {
+            button1.setText(correct_answer + "");
+            button2.setText(correct_answer + 1 + "");
+            button3.setText(correct_answer - 1  + "");
+            button4.setText(correct_answer + 2 +"");
+        }
+        else if(correctButton == 1)
+        {
+            button1.setText(correct_answer + 1 + "");
+            button2.setText(correct_answer + "");
+            button3.setText(correct_answer - 1  + "");
+            button4.setText(correct_answer + 2 +"");
+        }
+        else if(correctButton == 2)
+        {
+            button1.setText(correct_answer + 1 + "");
+            button2.setText(correct_answer - 1 + "");
+            button3.setText(correct_answer + "");
+            button4.setText(correct_answer + 2 +"");
+        }
+        else
+        {
+            button1.setText(correct_answer + 1 + "");
+            button2.setText(correct_answer - 1 + "");
+            button3.setText(correct_answer + 2 + "");
+            button4.setText(correct_answer + "");
+        }
+        if(matchCounter==3){    // if three matches are completed update the performance in sharedpreferences
             matchCounter=0;
-
             //adjusting the performance array so that last six entries present with the most recent at the last index.
             if (performance.length - 1 >= 0)
                 System.arraycopy(performance, 1, performance, 0, performance.length - 1);
             performance[5]=sumOfScore(); //calculating the sum of last three matches (note result of a match is 1 ro 0, and add to performance
             sharedPreferences.edit().putString("data",new Gson().toJson(performance)).apply();
-
         }
     }
-
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
         // your code here
         return 0;
     }
-
     public int[][] dataPrep() {
         int[] data = new Gson().fromJson((sharedPreferences.getString("data", null)), performance.getClass());
         Log.i("data", Arrays.toString(data)); //this is how you display arrays in Logcat, for debugging
@@ -103,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return dataFrame;
     }
-
     public String getInterpretation(int [][]dataFrame,double slope){
         //provide interpretation based on your slope analysis
         // Your code here
