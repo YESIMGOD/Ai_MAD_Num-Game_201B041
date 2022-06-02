1216b1052e2fb0b89f8e17123b49efc2c5af4119
YESIMGOD <ristoo2001@gmail.com>
2022-06-03 02:14:46 +0530

    Switch Added

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,29 +24,27 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
-    int matchCounter=0;
+    int matchCounter=0,c = 0,ic = 0;
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
     String[] operators ={"+","-","*","/"};
-    int correctButton=0; //which button will have the correct answer (tag of that button)
+    int correctButton= 0; //which button will have the correct answer (tag of that button)
     Random random=new Random(); //You will generate random algebra questions
-    TextView textView2;
+    TextView textView2,correct, incorrect;
     Button button1,button2,button3,button4;
-    public void load(View view)
-    {
+    public void load(View view){
         Button buttonClicked=(Button)view;
-        if(buttonClicked.getTag().toString().equals(correctButton+""))
-        {
+        if(buttonClicked.getTag().toString().equals(correctButton+"")){
             score[matchCounter++]=1;
-        }else
-        {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        }else{
             score[matchCounter++]=0;
+            Toast.makeText(this, "Inorrect", Toast.LENGTH_SHORT).show();
         }
         newMatch();
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1=findViewById(R.id.button1);
@@ -54,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
         button4=findViewById(R.id.button4);
         textView2=findViewById(R.id.textView2);
         newMatch();
-        sharedPreferences=this.getSharedPreferences("com.example.aiapp_2022", Context.MODE_PRIVATE);
-        int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimensional array
+        sharedPreferences=this.getSharedPreferences("com.example.ai_mad_numgame", Context.MODE_PRIVATE);
+        int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimenssional array
         double slope=LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
         new AlertDialog.Builder(this)
-                // .setIcon() //your custom icon
+                .setIcon(android.R.drawable.ic_dialog_info) //your custom icon
                 .setTitle("Performance")
 
                 .setMessage(getInterpretation(dataFrame,slope))
@@ -66,15 +65,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressLint("SetTextI18n")
-    public void newMatch() {  //A game is composed of three matches
+    public String newMatch() {  //A game is composed of three matches
+
 
         int operand1 = random.nextInt(10);
-        int operand2=random.nextInt(10);
-        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+        int operand2 = random.nextInt(10);
+        if(operand2==0)
+            operand2++;
+        correctButton = random.nextInt(4);
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-        // Your code here, to display correct and incorrect options on the buttons
         int correct_answer;
         switch (operator) {
             case "+":
@@ -119,20 +120,36 @@ public class MainActivity extends AppCompatActivity {
             button3.setText(correct_answer + 2 + "");
             button4.setText(correct_answer + "");
         }
+        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+
+
+        // Your code here, to display correct and incorrect options on the buttons
+
         if(matchCounter==3){    // if three matches are completed update the performance in sharedpreferences
+
             matchCounter=0;
-            //adjusting the performance array so that last six entries present with the most recent at the last index.
+
             if (performance.length - 1 >= 0)
                 System.arraycopy(performance, 1, performance, 0, performance.length - 1);
             performance[5]=sumOfScore(); //calculating the sum of last three matches (note result of a match is 1 ro 0, and add to performance
             sharedPreferences.edit().putString("data",new Gson().toJson(performance)).apply();
+
         }
+        return operator;
     }
+
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
+        int sum=0;
+
+        for(int i = 0; i < 3; i++)
+        {
+            sum += score[i];
+        }
         // your code here
-        return 0;
+        return sum;
     }
+
     public int[][] dataPrep() {
         int[] data = new Gson().fromJson((sharedPreferences.getString("data", null)), performance.getClass());
         Log.i("data", Arrays.toString(data)); //this is how you display arrays in Logcat, for debugging
@@ -145,9 +162,21 @@ public class MainActivity extends AppCompatActivity {
         }
         return dataFrame;
     }
+
     public String getInterpretation(int [][]dataFrame,double slope){
         //provide interpretation based on your slope analysis
-        // Your code here
-        return "Your Interpretation";
+        if(slope>1)
+            return "Good job, keep it up";
+        else if(slope<1)
+            return "Need to work more";
+        else {
+            int n=0;
+            for(int i=0;i<=5;i++){
+                if(dataFrame[i][1]==3) n++;
+            }
+            if(n==6)
+                return "Awesome, Perfect score of 3 in last 6 games";
+        }
+        return "Constant Performance";
     }
 }
