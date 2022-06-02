8abeeaa972c4c7847c8122c21d415eac8ad7eaa0
YESIMGOD <ristoo2001@gmail.com>
2022-06-03 00:01:23 +0530

    Resolved Code

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -3,11 +3,9 @@ package com.example.ai_mad_numgame;
    App will show your last performance at the start of the activity. New Tournament will start from
    all performance set to -1 again. And your new performance will be visible, when you return back to game
  */
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,21 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.gson.Gson;  //make changes at appropriate places to include this dependency
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     int matchCounter=0;
     int []performance={-1,-1,-1,-1,-1,-1}; //score of a game is updated in this array
     int []score={-1,-1,-1}; //score of each match is updated in this array. A total of three matches in a game
-    String operators[]={"+","-","*","/"};
+    String[] operators ={"+","-","*","/"};
     int correctButton=0; //which button will have the correct answer (tag of that button)
     Random random=new Random(); //You will generate randdom alegebra questions
     TextView textView2;
@@ -57,18 +54,14 @@ public class MainActivity extends AppCompatActivity {
         int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimenssional array
         double slope=LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
         new AlertDialog.Builder(this)
-               // .setIcon() //your custom icon
+                // .setIcon() //your custom icon
                 .setTitle("Performance")
 
                 .setMessage(getInterpretation(dataFrame,slope))
-                .setNeutralButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        newMatch();
-                    }
-                }).show();
+                .setNeutralButton("OK", (dialog, which) -> newMatch()).show();
     }
 
+    @SuppressLint("SetTextI18n")
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
@@ -77,15 +70,15 @@ public class MainActivity extends AppCompatActivity {
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+        // Your code here, to diplay correct and incorrect options on the buttons
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
             matchCounter=0;
 
-            for(int i=0;i<performance.length-1;i++){ //adjusting the performance array so that last six entries present with the most recent at the last index.
-                performance[i]=performance[i+1];
-            }
+            //adjusting the performance array so that last six entries present with the most recent at the last index.
+            if (performance.length - 1 >= 0)
+                System.arraycopy(performance, 1, performance, 0, performance.length - 1);
             performance[5]=sumOfScore(); //calculating the sum of last three matches (note result of a match is 1 ro 0, and add to performance
             sharedPreferences.edit().putString("data",new Gson().toJson(performance)).apply();
 
@@ -94,15 +87,14 @@ public class MainActivity extends AppCompatActivity {
 
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
-        int sum=0;
-       // your code here
-        return sum;
+        // your code here
+        return 0;
     }
 
     public int[][] dataPrep() {
         int[] data = new Gson().fromJson((sharedPreferences.getString("data", null)), performance.getClass());
         Log.i("data", Arrays.toString(data)); //this is how you display arrays in Logcat, for debugging
-        int dataFrame[][] = new int[6][2]; //creating a dataframe of two columns and six rows for regresson purpose
+        int[][] dataFrame = new int[6][2]; //creating a dataframe of two columns and six rows for regresson purpose
         if(data==null)
             return null;
         for (int i = 0; i < data.length; i++) {
@@ -113,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getInterpretation(int [][]dataFrame,double slope){
-       //provide interpretation based on your slope analysis
+        //provide interpretation based on your slope analysis
         // Your code here
         return "Your Interpretation";
     }
